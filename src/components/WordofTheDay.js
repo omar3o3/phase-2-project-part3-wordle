584bb0340bef873b356bb66c6477c69924fc9c2f
omar3o3 <otantawy53@gmail.com>
2022-05-29 23:46:10 -0400

    incorportated jabber but needs to be attached to oxford dictionary api

--- src/components/WordofTheDay.js
+++ src/components/WordofTheDay.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 function WordofTheDay() {
+
+  const Jabber = require('jabber');
+
+  const jabber = new Jabber();
+
+  // console.log(jabber.createWord(5))
+
   return (
-    <div>WordofTheDay</div>
+    <div className='text-center'>
+      <h1 className='m-5'>Word of The Day</h1>
+      <h3>{jabber.createWord(5 , true)}</h3>
+    </div>
   )
 }
 
