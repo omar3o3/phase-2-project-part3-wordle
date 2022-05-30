0e93452967c10edf33f3e2a1b89414366a02f9b6
omar3o3 <otantawy53@gmail.com>
2022-05-30 01:40:55 -0400

    removed jabber and added random-words instead, and dictionary api to fetch defintions from generated words

--- src/components/EasyMode.js
+++ src/components/EasyMode.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function EasyMode() {
+
   return (
     <div>EasyMode</div>
   )
--- src/components/WordofTheDay.js
+++ src/components/WordofTheDay.js
@@ -2,16 +2,41 @@ import React from 'react'
 
 function WordofTheDay() {
 
-  const Jabber = require('jabber');
+  let randomWords = require('random-words');
+  // console.log(randomWords());
 
-  const jabber = new Jabber();
+  const definitionFetch = () =>{
+    // fetch('https://api.dictionaryapi.dev/api/v2/entries/en/hello')
+    // fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`)
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWords()}`)
+    .then(resp => resp.json())
+    // .then(data => console.log(data))
+    .then(data => breakDownDef(data))
+  }
 
-  // console.log(jabber.createWord(5))
+  definitionFetch();
+  const breakDownDef = (data) =>{
+    // console.log(data.meanings.definitions)
+    // console.log(data.meanings[1]);
+    // console.log(data.word)
+    //to get all meanings
+    // console.log(data[0].meanings)
+    // to get one of the meanings
+    // console.log(data[0].meanings[0].definitions[0].definition)
+    // console.log(data[0].meanings[1].definitions[0].definition)
+    // console.log(data[0].meanings[2].definitions[0].definition)
+    console.log(data)
+
+    //need to iterate over the arrays within meanings key
+    //then iterate over the definitions within each definitions key
+    //create 2 callback functions to handle each iteration
+    //second or third cb will handle creating elements for the definition
+  }
 
   return (
     <div className='text-center'>
       <h1 className='m-5'>Word of The Day</h1>
-      <h3>{jabber.createWord(5 , true)}</h3>
+      <h3>{randomWords()}</h3>
     </div>
   )
 }
