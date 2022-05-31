2dfc82a5e15ab577979e7a4c49146c4dd779a18b
omar3o3 <otantawy53@gmail.com>
2022-05-31 14:44:26 -0400

    getting ready to create states for each row

--- src/components/EasyMode.js
+++ src/components/EasyMode.js
@@ -1,124 +1,136 @@
-import React , {useState ,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function EasyMode() {
 
-  const [guessValue , setGuessValue] = useState('')
+  const [guessValue, setGuessValue] = useState('')
+  const [previousGuesses , setPreviousGuesses] = useState([])
 
-  useEffect(()=>{
-    window.addEventListener('keyup' , handleKeyUp)
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyUp)
 
-    return () => window.removeEventListener('keyup' , handleKeyUp)
+    return () => window.removeEventListener('keyup', handleKeyUp)
   }, [guessValue])
 
-  const handleKeyUp = ({key}) =>{
 
-    // console.log(e.key)
-    // console.log(key);
+  const handleKeyUp = ({ key }) => {
+    if (/^[A-Za-z]$/.test(key)) {
+      if (guessValue.length < 5) {
+        setGuessValue(prev => prev + key)
+      }
+    }
 
-    setGuessValue((prev) => prev + key)
+    if (key === 'Backspace'){
+      setGuessValue(prev => prev = prev.slice(0, -1))
+    }
 
-    // setGuessValue(prev => [prev + key])
-    // console.log(guessValue);
+    if(key === 'Enter' && guessValue.length === 5){
+      setPreviousGuesses(prev => [...prev , guessValue])
+    }
 
-    // setGuessValue((prev) => [prev , e.key])
-    // console.log(guessValue)
   }
 
-  if (guessValue !== ''){
+
+
+  
+  if (guessValue !== '') {
     console.log(guessValue);
   }
-  // console.log(guessValue);
+
+  if(previousGuesses.length > 0){
+    console.log(previousGuesses);
+  }
+
 
   return (
     <div className='container'>
-      
+
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-dark bg-warning">
-          A
+        <div className="col border border-dark bg-warning" style={{height: '3.5rem'}}>
+          {guessValue[0] ? guessValue[0] : ''}
         </div>
-        <div className="col border border-dark bg-success">
-          B
+        <div className="col border border-dark bg-success" style={{height: '3.5rem'}}>
+        {guessValue[1]  ? guessValue[1] : ''}
         </div>
-        <div className="col border border-dark bg-secondary">
-          C
+        <div className="col border border-dark bg-secondary" style={{height: '3.5rem'}}>
+        {guessValue[2]  ? guessValue[2] : ''}
         </div>
-        <div className="col border border-dark">
-          D
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+        {guessValue[3]  ? guessValue[3] : ''}
         </div>
-        <div className="col border border-dark">
-          E
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+        {guessValue[4]  ? guessValue[4] : ''}
         </div>
       </div>
 
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-dark bg-warning">
-          A
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark bg-success">
-          B
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark bg-secondary">
-          C
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark">
-          D
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark">
-          E
-        </div>
-      </div>
-
-      <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-dark bg-warning">
-          A
-        </div>
-        <div className="col border border-dark bg-success">
-          B
-        </div>
-        <div className="col border border-dark bg-secondary">
-          C
-        </div>
-        <div className="col border border-dark">
-          D
-        </div>
-        <div className="col border border-dark">
-          E
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
       </div>
 
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-dark bg-warning">
-          A
+      <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark bg-success">
-          B
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark bg-secondary">
-          C
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark">
-          D
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark">
-          E
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
       </div>
 
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-dark bg-warning">
-          A
+      <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark bg-success">
-          B
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark bg-secondary">
-          C
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark">
-          D
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
-        <div className="col border border-dark">
-          E
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
+        </div>
+      </div>
+
+      <div className="row align-items-start m-5 display-5 text-center">
+      <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
+        </div>
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
+        </div>
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
+        </div>
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
+        </div>
+        <div className="col border border-dark" style={{height: '3.5rem'}}>
+          
         </div>
       </div>
 
