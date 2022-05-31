686c4fd56d562ae35dab731d6a0f0ffa1491c372
omar3o3 <otantawy53@gmail.com>
2022-05-31 12:50:15 -0400

    fixed setting guess value

--- src/components/EasyMode.js
+++ src/components/EasyMode.js
@@ -1,96 +1,123 @@
-import React from 'react'
+import React , {useState ,useEffect} from 'react'
 
 function EasyMode() {
+
+  const [guessValue , setGuessValue] = useState('')
+
+  useEffect(()=>{
+    window.addEventListener('keyup' , handleKeyUp)
+
+    return () => window.removeEventListener('keyup' , handleKeyUp)
+  }, [guessValue])
+
+  const handleKeyUp = ({key}) =>{
+
+    // console.log(e.key)
+    // console.log(key);
+
+    setGuessValue((prev) => prev + key)
+
+    // setGuessValue(prev => [prev + key])
+    // console.log(guessValue);
+
+    // setGuessValue((prev) => [prev , e.key])
+    // console.log(guessValue)
+  }
+
+  if (guessValue !== ''){
+    console.log(guessValue);
+  }
+  // console.log(guessValue);
 
   return (
     <div className='container'>
       
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-danger bg-warning">
+        <div className="col border border-dark bg-warning">
           A
         </div>
-        <div className="col border border-danger bg-success">
+        <div className="col border border-dark bg-success">
           B
         </div>
-        <div className="col border border-danger bg-secondary">
+        <div className="col border border-dark bg-secondary">
           C
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           D
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           E
         </div>
       </div>
 
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-danger bg-warning">
+        <div className="col border border-dark bg-warning">
           A
         </div>
-        <div className="col border border-danger bg-success">
+        <div className="col border border-dark bg-success">
           B
         </div>
-        <div className="col border border-danger bg-secondary">
+        <div className="col border border-dark bg-secondary">
           C
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           D
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           E
         </div>
       </div>
 
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-danger bg-warning">
+        <div className="col border border-dark bg-warning">
           A
         </div>
-        <div className="col border border-danger bg-success">
+        <div className="col border border-dark bg-success">
           B
         </div>
-        <div className="col border border-danger bg-secondary">
+        <div className="col border border-dark bg-secondary">
           C
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           D
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           E
         </div>
       </div>
 
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-danger bg-warning">
+        <div className="col border border-dark bg-warning">
           A
         </div>
-        <div className="col border border-danger bg-success">
+        <div className="col border border-dark bg-success">
           B
         </div>
-        <div className="col border border-danger bg-secondary">
+        <div className="col border border-dark bg-secondary">
           C
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           D
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           E
         </div>
       </div>
 
       <div className="row align-items-start m-5 display-5 text-center">
-        <div className="col border border-danger bg-warning">
+        <div className="col border border-dark bg-warning">
           A
         </div>
-        <div className="col border border-danger bg-success">
+        <div className="col border border-dark bg-success">
           B
         </div>
-        <div className="col border border-danger bg-secondary">
+        <div className="col border border-dark bg-secondary">
           C
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           D
         </div>
-        <div className="col border border-danger">
+        <div className="col border border-dark">
           E
         </div>
       </div>
