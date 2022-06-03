2467c7eb1095a0d75f1c7dc413cdd83b755c7e23
omar3o3 <otantawy53@gmail.com>
2022-06-02 21:40:59 -0400

    added button

--- src/components/EasyMode.js
+++ src/components/EasyMode.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import useGameMode from './useGameMode';
 import Grid from './Grid'
+import Button from 'react-bootstrap/Button'
 
 function EasyMode({ filteredSolution }) {
 
@@ -12,13 +13,20 @@ function EasyMode({ filteredSolution }) {
     return () => window.removeEventListener('keyup', handleKeyUp);
   }, [currentGuess, handleKeyUp])
 
+  const handleHint = () =>{
+    
+  }
+
   return (
-    <div className='my-5'>
+    <div className='my-5 justify-content-center'>
       <div className='text-center display-4 mb-2'>
         {gameWon ? (`You won! It took you ${turnValue === 1 ? '1 try.' : `${turnValue} tries.`}`) : null}
       </div>
       <div className='text-center display-4 mb-2'>
-      {gameLost ? `Better luck next time, the solution is ${filteredSolution}` : null}
+        {gameLost ? `Better luck next time, the solution is ${filteredSolution}` : null}
+      </div>
+      <div className='text-center my-2'>
+        <Button onClick={handleHint} variant="outline-dark">Need a hint?</Button>
       </div>
       <Grid currentGuess={currentGuess} oldGuesses={oldGuesses} turnValue={turnValue} />
     </div>
