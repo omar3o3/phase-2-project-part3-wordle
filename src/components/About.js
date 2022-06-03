a05548ac96e407cf0b158f7f4d2c30797bd32905
JamesNg <njames15@gmail.com>
2022-06-03 11:51:31 -0400

    about updates and merge ready

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,28 @@ function About() {
         </div>
         <div style={{ flex: 1, height: '1px', backgroundColor: 'black' }} />
       </div>
-      <p className='text-center fs-3'>This is a project that is meant to recreate the popular, contemporary game called 'Wordle'.</p>
+      <p className='text-center fs-4'>
+        This is a project meant to recreate the popular, contemporary game called 'Wordle'.
+      </p>
+      <br />
+      <p className='text-center fw-semibold fs-3'>
+        How To Play
+      </p>
+      <div className="d-flex">
+        <ol className="mx-auto justify-content-center fs-5">
+          <li>Enter a 5 letter word.</li>
+          <li>Upon submitting a guess, you will see different colors around the tiles.</li>
+            <ul>
+              <li><span className="fw-semibold px-2" style={{ backgroundColor: '#75EF7D' }}>Green Tile</span> - the character is in the word and IS in the correct place.</li>
+              <li><span className="fw-semibold px-2" style={{ backgroundColor: 'yellow' }}>Yellow Tile</span> - the character is in the word, but it is NOT in the correct place.<br /> There can also be two or more of this character.</li>
+              <li><span className="fw-semibold px-2" style={{ backgroundColor: '#D5D2D2' }}>Grey Tile</span> - the character is NOT in the word.</li>
+            </ul>
+          <li>You have 6 attempts. Good luck!</li>
+        </ol>
+      </div>
+
+      <br /><br /><br />
+
       <Liker />
     </div>
   )
