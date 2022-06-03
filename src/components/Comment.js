9b98ae48978b571cf63da2e2eb31a45d9fd7c838
omar3o3 <otantawy53@gmail.com>
2022-06-03 12:59:26 -0400

    fixed comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
 import CardGroup from 'react-bootstrap/CardGroup'
-import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
@@ -11,62 +10,27 @@ function Comment({ comments }) {
 
     if (comments) {
         return (
-            <Container fluid>
+            <div>
                 {commentsArray.map(comment => (
-                    <Row className="justify-content-center" key={comment.id}>
-                        <Col >
-                            <CardGroup >
-                                <Card style={{flex: 1}}>
-                                    <Card.Body>
-                                        <Card.Title className='lead fs-3 fw-bold'>{comment.name}</Card.Title>
-                                        <Card.Text className='lead fs-4'>
-                                            {comment.comment}
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </CardGroup>
+                    <Row sm={2} className='justify-content-center my-2'>
+                        <Col>
+                            <Card key={comment.id} className='shadow-sm'>
+                                <Card.Body>
+                                    <Card.Title className='lead fs-3 fw-bold'>{comment.name}</Card.Title>
+                                    <Card.Text className='lead fs-4'>
+                                        {comment.comment}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         </Col>
                     </Row>
                 ))}
-            </Container>
+            </div>
         )
     }
 
-    // <Col>
-    // <CardGroup>
-    //     <Card>
-    //         <Card.Body>
-    //             <Card.Title>{name}</Card.Title>
-    //             <Card.Text>
-    //                 {comment}
-    //             </Card.Text>
-    //         </Card.Body>
-    //     </Card>
-    // </CardGroup>
-    // </Col>
-
     return (
-        // <div className='border-top border-primary my-2'>
-        //     <p className='lead justify-content-center my-3'>"{comment}" -{name}</p>
-        // </div>
-
-        // <Container>
-        //     <Row>
-        //         <Col>
-        //             <CardGroup>
-        //                 <Card>
-        //                     <Card.Body>
-        //                         <Card.Title></Card.Title>
-        //                         <Card.Text>
-        //                         </Card.Text>
-        //                     </Card.Body>
-        //                 </Card>
-        //             </CardGroup>
-        //         </Col>
-        //     </Row>
-        // </Container>
-
-        <div className='d-flex justify-content-center text-center'>
+        <div>
 
         </div>
     )
